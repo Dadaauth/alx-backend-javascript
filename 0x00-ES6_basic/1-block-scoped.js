b53d4e3fcaed580b2d1d8c13fde7893e555b774b
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-22 14:58:14 +0100

    hoping to solve

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,14 @@
 export default function taskBlock(trueOrFalse) {
+  function getTask(task, task2) {
+    return [task, task2];
+  }
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
-    function getTask() {
-      return [task, task2];
-    }
+    getTask(task, task2);
   }
 
   return [task, task2];
