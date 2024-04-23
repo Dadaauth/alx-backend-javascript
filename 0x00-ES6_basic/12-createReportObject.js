cff1a8fc65cc67247314b351a92179650a405e2e
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-23 15:54:34 +0100

    task #12 done

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,11 @@
 export default function createReportObject(employeesList) {
-
+  const obj = {};
+  obj['allEmployees'] = {};
+  for (const [key, value] of Object.entries(employeesList)) {
+      obj['allEmployees'][key] = value;
+  }
+  obj['getNumberOfDepartments'] = () => {
+    return Object.keys(employeesList).length;
+  }
+  return obj;
 }
