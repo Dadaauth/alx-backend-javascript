ffb98c96544e3a955647e433eb35be7ebf14a387
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-23 19:11:56 +0100

    task #1 done

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,6 @@
+export default function getFullResponseFromAPI(success) {
+  return success? Promise.resolve({
+    status: 200,
+    body: 'Success'
+  }) : Promise.reject(new Error("The fake API is not working currently"));
+}
