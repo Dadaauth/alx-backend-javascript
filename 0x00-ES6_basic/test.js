72157c87ed9d4dd5e8d2397cab308ebb420cdba3
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-23 11:26:22 +0100

    task #11 done

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,3 +1,3 @@
-import getSanFranciscoDescription from './6-string-interpolation.js';
+import createEmployeesObject from './11-createEmployeesObject.js';
 
-console.log(getSanFranciscoDescription());
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
