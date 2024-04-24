c0f0edc5d82ded96e12761e742b21a68de5bff34
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-24 17:23:12 +0100

    fixed eslint error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  try{
+  try
+  {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }
-  catch (error){
+  catch (error)
+  {
     console.log('Signup system offline');
   }
 }
