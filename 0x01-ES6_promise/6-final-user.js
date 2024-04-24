f2f3a16170f9a3d03416b7653e2e8edcce5245ea
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-24 17:47:41 +0100

    task #7 done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return await Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+  return await Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
