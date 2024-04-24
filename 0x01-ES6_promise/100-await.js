dda0a97117f0a69eb9ed7fbabf73bfd9e845bfde
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-24 22:57:09 +0100

    Fix eslint error

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from "./utils";
 
 export default async function asyncUploadUser() {
-    const toRtn = {};
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    if (photo) toRtn.photo = photo;
-    else toRtn.photo = null;
-    if (user) toRtn.user = user;
-    else toRtn.user = null;
-    return toRtn;
+  const toRtn = {};
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  if (photo) toRtn.photo = photo;
+  else toRtn.photo = null;
+  if (user) toRtn.user = user;
+  else toRtn.user = null;
+  return toRtn;
 }
