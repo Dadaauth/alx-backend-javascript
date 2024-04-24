b7d416c1bc21aa5a60f8caa884ecb190821eb925
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-24 22:48:15 +0100

    fixed linter error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
-import signUpUser from './4-user-promise.js'; // Assuming 4-user-promise.js is in the same directory
-import uploadPhoto from './5-photo-reject.js'; // Assuming 5-photo-reject.js is in the same directory
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const answer = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   for (const v of answer) {
     if (v.status === 'rejected') {
-        v.value = `Error: ${v.reason.message}`;
-        delete v.reason;
+      v.value = `Error: ${v.reason.message}`;
+      delete v.reason;
     }
   }
   return answer;
