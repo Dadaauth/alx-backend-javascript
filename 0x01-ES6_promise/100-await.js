6adaa714252476c449a36daaa6588018ef9d1baa
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-24 23:00:57 +0100

    fix eslitn and a bug

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,15 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const toRtn = {};
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  if (photo) toRtn.photo = photo;
-  else toRtn.photo = null;
-  if (user) toRtn.user = user;
-  else toRtn.user = null;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    toRtn.photo = photo;
+    toRtn.user = user;
+  } catch (err) {
+    toRtn.photo = null;
+    toRtn.user = null;
+  }
   return toRtn;
 }
