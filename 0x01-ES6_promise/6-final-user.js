96c8049ff18e73b2e0aedc394d9eb40d040688f6
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-24 22:41:30 +0100

    task #6 corrected

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,13 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js'; // Assuming 4-user-promise.js is in the same directory
+import uploadPhoto from './5-photo-reject.js'; // Assuming 5-photo-reject.js is in the same directory
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const answer = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  for (const v of answer) {
+    if (v.status === 'rejected') {
+        v.value = `Error: ${v.reason.message}`;
+        delete v.reason;
+    }
+  }
+  return answer;
 }
