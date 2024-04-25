f8dc48e64398ccd3d6716a326f77d4e4201ca255
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-25 18:22:43 +0100

    task #2 done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,33 @@
+
+export default class HolbertonClass {
+  constructor (name, length, students) {
+    if (typeof name === 'string') this._name = name;
+    else throw TypeError('Name must be a string');
+    if (typeof length === 'number') this._length = length;
+    else throw TypeError('Length must be a number');
+    for (const student of students) if (typeof student !== 'string') throw TypeError('Students should be an array of strings');
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+  set length(value) {
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+  set students(value) {
+    this._students = value;
+  }
+
+}
