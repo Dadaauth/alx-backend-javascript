8f5730cf045296fda458492cc5237188956eef6f
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-26 16:01:25 +0100

    Fixed eslitn

--- 0x02-ES6_classes/test.js
+++ 0x02-ES6_classes/test.js
@@ -1,4 +1,8 @@
 import Currency from "./3-currency.js";
 
 const dollar = new Currency('$', 'Dollars');
+console.log(dollar.code);
+console.log(dollar.name);
+dollar.code = '@'
+dollar.name = 'Clement'
 console.log(dollar.displayFullCurrency());
