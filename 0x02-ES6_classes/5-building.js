b2acecb69e6875e148e76d099e86664e6269bf5c
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-26 17:02:26 +0100

    Fixed linter error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,11 +3,11 @@ export default class Building {
     // if (this.constructor === Building) {
     //   throw new Error('Class is of abstract type and can\'t be instantiated');
     // }
-    if (this.constructor !== Building && this.evacuationWarningMessage == undefined) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
-  get sqft() { return this._sqft; } 
+  get sqft() { return this._sqft; }
 }
