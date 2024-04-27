f804e7701e26cc2156596a9e5305b7de5cd0b5df
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-27 16:00:44 +0100

    Fixed some things in task #11 file

--- 0x02-ES6_classes/test.js
+++ 0x02-ES6_classes/test.js
@@ -1,16 +1,16 @@
 import EVCar from './100-evcar.js';
 import Car from './10-car.js';
 
-test("Species is correctly implemented on EVCar", () => {
-    const tesla = new EVCar('Tesla', 'Electric', 'Red', '250');
-    const newCar = tesla.cloneCar();
+// test("Species is correctly implemented on EVCar", () => {
+//     const tesla = new EVCar('Tesla', 'Electric', 'Red', '250');
+//     const newCar = tesla.cloneCar();
 
-    expect(newCar instanceof EVCar).toBe(false);
-    expect(newCar instanceof Car).toBe(true);
-});
+//     expect(newCar instanceof EVCar).toBe(false);
+//     expect(newCar instanceof Car).toBe(true);
+// });
 
-// const tesla = new EVCar('Tesla', 'Electric', 'Red', '250');
-// const newCar = tesla.cloneCar();
+const tesla = new EVCar('Tesla', 'Electric', 'Red', '250');
+const newCar = tesla.cloneCar();
 
-// console.log(newCar instanceof EVCar);
-// console.log(newCar instanceof Car);
+console.log(newCar instanceof EVCar);
+console.log(newCar instanceof Car);
