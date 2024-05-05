345ec8e1f8c6d12d8e3c665a39ff052194522e1b
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-05 16:57:41 +0100

    Update

--- 0x03-ES6_data_manipulation/test.js
+++ 0x03-ES6_data_manipulation/test.js
@@ -1,4 +1,16 @@
-import cleanSet from "./8-clean_set.js";
-
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+var arrs = [1, 12, "string", true];
+var someValue = "this is a string";
+var strLength = someValue.length;
+var men = "Dojo";
+// const req = { url: "https://example.com", method: "GET" };
+// handleRequest(req.url, req.method);
+// Change 1:
+// const req = { url: "https://example.com", method: "GET" as "GET" };
+// Change 2
+// handleRequest(req.url, req.method as "GET");
+var req = { url: "https://example.com", method: "GET" };
+handleRequest(req.url, req.method);
+var firstName = Symbol("name");
+var secondName = Symbol("name");
+console.log(typeof firstName);
+console.log(typeof secondName);
