491864f37921cb0162f0bb9b22f89f0175406c84
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-08-29 14:35:55 +0100

    Done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,18 @@
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on("data", (input) => {
-  process.stdout.write(`Your name is: ${input}`);
+process.stdin.setEncoding('utf8');
+console.log('Welcome to Holberton School, what is your name?');
+
+let data = "";
+
+process.stdin.on('data', (chunk) => {
+  data += chunk;
 });
-process.on("SIGINT", () => {
-    process.stdout.write("This important software is now closing");
-    process.exit(0);
+
+process.on('SIGINT', () => {
+  process.stdout.write('This important software is now closing');
+  process.exit(0);
 })
+
+process.stdin.on('end', () => {
+  process.stdout.write(`Your name is: ${input}`);
+  process.stdout.write('This important software is now closing');
+});
