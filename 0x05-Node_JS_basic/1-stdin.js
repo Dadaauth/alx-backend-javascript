3f4c0fdc0a89a2ff59e5e3340913c5af3590f1a4
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-08-29 14:17:51 +0100

    Task #1 done

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin.on("data", (input) => {
+  process.stdout.write(`Your name is: ${input}`);
+});
+process.on("SIGINT", () => {
+    process.stdout.write("This important software is now closing");
+    process.exit(0);
+})
