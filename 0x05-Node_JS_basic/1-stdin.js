26b8d69de33ab94c7b1c7c01f888657b55921884
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-08-29 14:41:01 +0100

    Fixed linter error and minor issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
-let data = "";
+let data = '';
 
 process.stdin.on('data', (chunk) => {
   data += chunk;
@@ -10,9 +10,9 @@ process.stdin.on('data', (chunk) => {
 process.on('SIGINT', () => {
   process.stdout.write('This important software is now closing');
   process.exit(0);
-})
+});
 
 process.stdin.on('end', () => {
   process.stdout.write(`Your name is: ${data}`);
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
